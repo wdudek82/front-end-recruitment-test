97341c7aab681fc3376bff9b067970013f2f9e85
Wojtek Dudek <wdudek82@gmail.com>
2018-06-26 23:14:05 +0200

    Added regexes to personal information section of the checkout form

--- app/scripts/checkout.js
+++ app/scripts/checkout.js
@@ -17,8 +17,8 @@ function handleFormSubmit(e) {
   const securityCode = e.target.elements['security-code'];
   const expirationDate = e.target.elements['expiration-date'];
 
-  validateFirstName(fname);
-  validateLastName(lname);
+  validateName(fname);
+  validateName(lname);
   validateEmail(email);
   validatePhone(phone);
   validatePostalCode(postalCode);
@@ -28,45 +28,102 @@ function handleFormSubmit(e) {
 }
 
 /**
-* @param {string} fname - first name
+* @param {EventObject} element - input or select field
+* @param {string} className - class name to append to the element
 */
-function validateFirstName(fname) {
-  console.log(fname);
-  fname.classList.add('invalid');
+function outputValidationResults(element, className) {
+  if (className === 'invalid') {
+    element.classList.remove('valid');
+  } else if (className === 'valid') {
+    element.classList.remove('invalid');
+  }
+
+  element.classList.add(className);
+
+  // setTimeout(() => {
+  //   element.classList.remove(className);
+  // }, 1000);
 }
 
-function validateLastName(lname) {
-  console.log(lname);
-  
-  lname.classList.add('invalid');
+/**
+* @param {string} name - first or last name
+*/
+function validateName(name) {
+  const nameRegex = /^[A-Za-z]{3,20}$/;
+
+  let appendedClass = 'invalid';
+
+  if (name.value.match(nameRegex)) {
+    appendedClass = 'valid';
+  }
+
+  outputValidationResults(name, appendedClass);
 }
 
+/**
+* @param {string} email - email
+*/
 function validateEmail(email) {
-  console.log(email);
-  email.classList.add('invalid');
+  const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  let appendedClass = 'invalid';
+
+  if (email.value.match(emailRegex)) {
+    appendedClass = 'valid';
+  }
+
+  outputValidationResults(email, appendedClass);
 }
 
+/**
+* @param {string} postalCode - postalCode
+*/
 function validatePostalCode(postalCode) {
-  console.log(postalCode);
-  postalCode.classList.add('invalid');
+  const postalCodeRegex = /^[0-9]{5}$/;
+
+  let appendedClass = 'invalid';
+
+  if (postalCode.value.match(postalCodeRegex)) {
+    appendedClass = 'valid';
+  }
+
+  outputValidationResults(postalCode, appendedClass);
 }
 
+/**
+* @param {string} phone - phone
+*/
 function validatePhone(phone) {
-  console.log(phone);
-  phone.classList.add('invalid');
+  const phoneRegex = /^(\(\d{1,3}\)\s)?\(?\d{3}\)?[\s.-]\d{2}[\s.-]\d{2}$/;
+
+  let appendedClass = 'invalid';
+
+  if (phone.value.match(phoneRegex)) {
+    appendedClass = 'valid';
+  }
+
+  outputValidationResults(phone, appendedClass);
 }
 
+/**
+* @param {number} creditCardNumber - creditCardNumber
+*/
 function validateCreditCardNumber(creditCardNumber) {
   console.log(fname);
   creditCardNumber.classList.add('invalid');
 }
 
-
+/**
+* @param {string} securityCode - securityCode
+*/
 function validateSecurityCode(securityCode) {
   console.log(fname);
   securityCode.classList.add('invalid');
 }
 
+/**
+* @param {string} expirationDate - expirationDate
+*/
 function validateExpirationDate(expirationDate) {
   console.log(fname);
   expirationDate.classList.add('invalid');
